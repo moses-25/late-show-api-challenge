1ee8a314fd76b22fc5df5b2a526cc61d74b9bec3
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-06-23 09:50:00 +0300

    modified user.py

--- late-show-api-challenge/server/models/user.py
+++ late-show-api-challenge/server/models/user.py
@@ -1,9 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
+from server.models import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SQLAlchemy()
-
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
@@ -13,3 +13,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+        }
+
